2e707892f93f047fb1dee70e9a25f9567634b90a
jackdugan <duganjon@oregonstate.edu>
2023-11-26 13:12:06 -0500

    11/26

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
 if __name__ == '__main__':
-    emoji = requests.get('http://127.0.0.1:5000/fetch_emoji?name=iuhiluh').json()
+    emoji = requests.get('http://127.0.0.1:5000/fetch_emoji?name=guitar').json()
     print(emoji['emoji'])
