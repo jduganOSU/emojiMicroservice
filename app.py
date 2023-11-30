86fe147ff53a35985c799f6045df387ee391ea33
jackdugan <duganjon@oregonstate.edu>
2023-11-30 16:37:34 -0500

    11/30

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 from fetch_emoji import fetch_emoji
+import flask_cors
 
 app = Flask(__name__)
+flask_cors.CORS(app)
 
-@app.route('/fetch_emoji')
+
+@app.route('/fetch-emoji')
 def emoji_return():
     name = request.args.get('name', default='', type=str)
     emoji = fetch_emoji(name)
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import requests
 
 if __name__ == '__main__':
-    emoji = requests.get('http://127.0.0.1:5000/fetch_emoji?name=guitar').json()
-    print(emoji['emoji'])
+    print('hello')
